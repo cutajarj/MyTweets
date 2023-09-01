ac5d4228fe0400b75ebeb1786407db3aad41671b
cutajarj2 <james.cutajar@paddypowerbetfair.com>
2023-09-01 17:01:02 +0100

    ref trans example

---
+++ PyTweets/functional_immutability.py
@@ -0,0 +1,14 @@
+
+
+letters = ["a", "b", "c"]
+
+letters.append("d")
+letters.append("e")
+print(letters)
+
+letters_tupple = ("a", "b", "c")
+
+letters_tupple2 = letters_tupple + ("d", "e")
+
+print(letters_tupple2)
+print(letters_tupple)
